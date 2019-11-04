258eb25c378ad38bc26a98da99409328b3336e02
bitm-regex <regex.bitm@gmail.com>
2019-11-04 09:41:04 +0600

    Items setup

---
+++ Zabin.project/StockManagementSystem/StockManagementSystem/Items.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockManagementSystem
+{   [Serializable]
+    public class Items
+    {
+        public string ItemName;
+        public string ReorderLevel;
+    }
+}
---
+++ Zabin.project/StockManagementSystem/StockManagementSystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StockManagementSystem.Startup))]
+namespace StockManagementSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
